87b13e34a114b9d31849335aab0adf6c424e76ce
silauras <olegpensov@gmail.com>
2021-01-07 17:03:43 +0200

    Trying request parameters

--- src/main/java/com/silauras/controllers/FirstController.java
+++ src/main/java/com/silauras/controllers/FirstController.java
@@ -3,6 +3,9 @@ package com.silauras.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by Silauras on 07.01.2021 at 15:31
@@ -13,12 +16,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+
+        System.out.println("Hello, " + name + " " + surname);
+
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodbyePage(){
+    public String goodbyePage() {
         return "first/goodbye";
     }
 }
